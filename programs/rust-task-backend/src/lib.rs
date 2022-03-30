8db81f69610a172610f3922a91e30654a78354d7
Andrew Strigaliov <strigaliov4working@gmail.com>
2022-03-30 11:30:24 +0300

    Added functionality

--- programs/rust-task-backend/src/lib.rs
+++ programs/rust-task-backend/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
+use anchor_lang::solana_program::system_instruction;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -6,10 +8,88 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod rust_task_backend {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+        let donators = &mut ctx.accounts.donators;
+        donators.total_donators = 0;
+        Ok(())
+    }
+
+    pub fn send_transaction(ctx: Context<SendTransaction>, amount: u64) -> ProgramResult {
+        let donators = &mut ctx.accounts.donators;
+        let user = &mut ctx.accounts.user;
+
+        system_instruction::transfer(
+            user.to_account_info().key,
+            donators.to_account_info().key,
+            amount,
+        );
+
+        let donator = DonatorStruct {
+            amount: amount,
+            address: *user.to_account_info().key,
+        };
+
+        donators.total_donators += 1;
+        donators.donators_list.push(donator);
+
+        Ok(())
+    }
+
+    pub fn receive_transaction(ctx: Context<ReceiveTransaction>) -> ProgramResult {
+        let donators = &mut ctx.accounts.donators;
+        let owner = donators.to_account_info().owner;
+        let user = &mut ctx.accounts.user;
+
+        if user.to_account_info().key != &owner.key() {
+            Err("You are not the owner!")
+        } else {
+            system_instruction::transfer(
+                donators.to_account_info().key,
+                user.to_account_info().key,
+                donators.donators_list[0].amount,
+                // donators.to_account_info().lamports(),
+            );
+            Ok(())
+        };
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer=user,space=9000)]
+    pub donators: Account<'info, Donators>,
+
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct SendTransaction<'info> {
+    #[account(mut)]
+    pub donators: Account<'info, Donators>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct ReceiveTransaction<'info> {
+    #[account(mut)]
+    pub donators: Account<'info, Donators>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct DonatorStruct {
+    pub amount: u64,
+    pub address: Pubkey,
+}
+
+#[account]
+pub struct Donators {
+    pub total_donators: u64,
+    pub donators_list: Vec<DonatorStruct>,
+}
--- tests/rust-task-backend.ts
+++ tests/rust-task-backend.ts
@@ -1,16 +1,43 @@
 import * as anchor from "@project-serum/anchor";
+import assert from 'assert';
 import { Program } from "@project-serum/anchor";
 import { RustTaskBackend } from "../target/types/rust_task_backend";
 
-describe("rust-task-backend", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
-
+describe('rust_task', () => {
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
   const program = anchor.workspace.RustTaskBackend as Program<RustTaskBackend>;
+  const { SystemProgram } = anchor.web3;
+  const donators = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+    await program.rpc.initialize({ 
+      accounts: {
+        donators: donators.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [donators],
+    });
+    const account = await program.account.donators.fetch(donators.publicKey);
+    assert.ok(Number(account.totalDonators) === 0);
   });
+
+  it("Is should send transaction", async () => {
+    await program.rpc.sendTransaction(new anchor.BN(30000),{ 
+      accounts: {
+        donators: donators.publicKey,
+        user: provider.wallet.publicKey,
+      },
+    });
+    const account = await program.account.donators.fetch(donators.publicKey);
+    assert.ok(Number(account.donatorsList[0].amount) === 30000);
+  })
+
+  it('Is should receive money', async () => {
+    await program.rpc.receiveTransaction({accounts: {donators: donators.publicKey, user: provider.wallet.publicKey}});
+    const account = await program.account.donators.fetch(donators.publicKey);
+    console.log(account);
+  })
+
 });
